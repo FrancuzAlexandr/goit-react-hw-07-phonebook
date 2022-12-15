3e1e3605ed4ba3003e056f2ee341e8e312dbec36
FrancuzAlexandr <francuz_s@ukr.net>
2022-12-15 22:24:35 +0200

    fix App LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,17 @@ import { ContactList } from './ContactList/ContactList';
 import { nanoid } from 'nanoid';
 
 export function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
+
   const [filter, setFilter] = useState('');
 
   const handleSubmit = ({ name, number }) => {
@@ -29,12 +34,12 @@ export function App() {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  useEffect(() => {
-    const LS = JSON.parse(localStorage.getItem('contacts'));
-    if (LS) {
-      setContacts(LS);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const LS = JSON.parse(localStorage.getItem('contacts'));
+  //   if (LS) {
+  //     setContacts(LS);
+  //   }
+  // }, []);
 
   const handleFilter = e => {
     setFilter(e.target.value.trim().toLocaleLowerCase());
